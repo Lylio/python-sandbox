7abe9a3fe64fd15cf38f1cd6fe9630712d55d141
Lyle Christine <cxz@gmx.com>
2018-10-20 11:09:38 +0100

    Update

--- Absolute Beginner Ch. 3/CrapsRoller.py
+++ Absolute Beginner Ch. 3/CrapsRoller.py
@@ -2,12 +2,12 @@
 # Demonstrates random number generation
 import random
 
+input("Press enter to roll die one: ")
 die1 = random.randint(1,6)
+
+input("Press enter to roll die two: ")
 die2 = random.randint(1,6)
+
 total = die1 + die2
 
-input("Press enter to roll die 1: ")
-
-input("Press enter to roll die 2: ")
-
-print("Die 1 rolled a " + str(die1) + " and die 2 rolled a " + str(die2) + " - your total is " + str(total))
+print("Die one rolled a " + str(die1) + " and die two rolled a " + str(die2) + " - your total is " + str(total))
