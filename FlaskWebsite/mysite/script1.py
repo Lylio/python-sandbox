94b3a945fb62955a5f3eac9956a559ef23039dd2
Lyle Christine <cxz@gmx.com>
2018-12-19 05:44:01 +0000

    Added comment on how to run app

--- FlaskWebsite/mysite/script1.py
+++ FlaskWebsite/mysite/script1.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+#To view the app, run script1.py and open browser at http://127.0.0.1:5000/
+
 @app.route('/')
 def home():
     return render_template("home.html")
