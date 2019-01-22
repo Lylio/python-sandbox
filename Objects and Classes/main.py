5650495184ef068e40f7df16108c24990d2d2abd
Lyle Christine <cxz@gmx.com>
2019-01-22 22:26:17 +0000

    Objects and Classes

---
+++ Objects and Classes/main.py
@@ -0,0 +1,13 @@
+from vehicles import Car, Motorcycle
+
+car1 = Car("Ford", "blue")
+car2 = Car("Honda", "red")
+car3 = Car("Volkswagon", "green")
+
+bike1 = Motorcycle(200, "pink")
+bike2 = Motorcycle(175, "grey")
+
+
+car2.display()
+bike1.display()
+
