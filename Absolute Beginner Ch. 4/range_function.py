dbd485147474515226cd88abf0cf610f36a131f3
Lyle Christine <cxz@gmx.com>
2019-01-25 20:20:30 +0000

    Update

--- Absolute Beginner Ch. 4/range_function.py
+++ Absolute Beginner Ch. 4/range_function.py
@@ -9,6 +9,6 @@ print("\n\nCounting in 5s")
 for i in range(0, 50, 5):
     print(i, end=" ")
 
-print("Counting backwards: ")
+print("\n\nCounting backwards: ")
 for i in range(10, 0, -1):
     print(i, end=" ")
