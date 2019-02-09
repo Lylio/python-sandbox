92c1e7c3681ff73e4be11743aa13a36d0e2a0c84
Lyle Christine <cxz@gmx.com>
2019-02-09 17:13:19 +0000

    update

--- Automate-the-Boring-Stuff/Ch.7/RegEx.py
+++
@@ -1,5 +0,0 @@
-import re
-
-phoneNumberRegEx = re.compile(r'\d\d\d-\d\d\d-\d\d\d')
-matchObject = phoneNumberRegEx.search('My number is 465-465-384.')
-print('Phone number found: ' + matchObject.group())
---
+++ Automate-the-Boring-Stuff/Web Scraping/mapIt.py
@@ -0,0 +1,14 @@
+#! python3
+
+import webbrowser
+import pyperclip
+import sys
+
+if len(sys.argv) > 1:
+    # Get address from command line
+    address = ' '.join(sys.argv[1:])
+else:
+    # Get address from clipboard
+    address = pyperclip.paste()
+
+webbrowser.open('https://www.google.com/maps/place/' + address)
