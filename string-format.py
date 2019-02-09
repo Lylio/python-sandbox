a8199866688cfc612040c0a68f5b3ec12da03a8d
Lyle Christine <cxz@gmx.com>
2019-02-09 09:22:11 +0000

    Update

---
+++ string-format.py
@@ -0,0 +1,13 @@
+# Practice using the str.format() function
+
+
+s = ('Sammy has {} balloons'.format(5))
+
+s2 = ('The {0} apples grow {1} in the {2}.'.format('green', 'well', 'Summer'))
+
+
+
+if __name__ == '__main__':
+    print(s)
+    print('\n-----------------\n')
+    print(s2)
