9b6da9151434b520f2348dd112adf5726dd8fc3b
Lyle Christine <cxz@gmx.com>
2019-02-09 19:13:49 +0000

    Update

---
+++ join-function.py
@@ -0,0 +1,18 @@
+# Practice with the str.join() function
+
+hyphen = '-'
+colours = ('blue', 'red', 'yellow', 'orange')
+
+print(hyphen.join(colours)) # blue-red-yellow-orange
+
+space = ' '
+days = ('mon', 'tue', 'wed', 'thur', 'fri')
+print(space.join(days)) # mon tue wed thur fri
+
+dot = '.'
+vowels = ('a', 'e', 'i', 'o', 'u')
+print(dot.join(vowels)) # a.e.i.o.ugit
+
+
+
+
